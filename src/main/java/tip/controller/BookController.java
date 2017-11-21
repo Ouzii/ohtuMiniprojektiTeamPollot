52854b9f2ca270ba90c7f8a9fd42dd3ce4a9b017
Viiala Tommi V V <twviiala@da3-cs-dk110-03.pc.helsinki.fi>
2017-11-21 17:02:06 +0200

    springsprongdeletemapping

--- src/main/java/tip/controller/BookController.java
+++ src/main/java/tip/controller/BookController.java
@@ -1,10 +1,11 @@
-
 package tip.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import tip.domain.Book;
@@ -12,20 +13,28 @@ import tip.repository.BookRepository;
 
 @Controller
 public class BookController {
-    
+
     @Autowired
     private BookRepository bookRepository;
-    
+
     @GetMapping("/")
     public String list(Model model) {
         model.addAttribute("tips", bookRepository.findAll());
         return "index";
     }
-    
+
     @PostMapping("/")
     public String add(@RequestParam String name, @RequestParam String isbn) {
         Book book = new Book(name, isbn);
         this.bookRepository.save(book);
         return "redirect:/";
     }
+
+    @DeleteMapping("/{tipId}")
+    public String delete(@PathVariable Long tipId) {
+        bookRepository.delete(bookRepository.getOne(tipId));
+        
+        return "redirect:/";
+    }
 }
+
