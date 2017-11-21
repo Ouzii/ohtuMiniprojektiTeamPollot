109ec1dc606cc6323e98678cabc90967adcddecc
Viiala Tommi V V <twviiala@da3-cs-dk110-03.pc.helsinki.fi>
2017-11-21 17:24:10 +0200

    tagrepo

--- src/main/java/tip/repository/TagRepository.java
+++ src/main/java/tip/repository/TagRepository.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tip.repository;
 
-/**
- *
- * @author twviiala
- */
-public interface TagRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import tip.domain.Tag;
+
+public interface TagRepository extends JpaRepository<Tag, Long> {
+
 }
