ff84066af035154c1627d41edc61d2546587c00e
Viiala Tommi V V <twviiala@da3-cs-dk110-03.pc.helsinki.fi>
2017-11-21 16:33:14 +0200

    springsprong

--- src/main/java/tip/controller/BookController.java
+++ src/main/java/tip/controller/BookController.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import tip.domain.Book;
 import tip.repository.BookRepository;
 
 @Controller
@@ -16,7 +18,14 @@ public class BookController {
     
     @GetMapping("/")
     public String list(Model model) {
-        
+        model.addAttribute("tips", bookRepository.findAll());
         return "index";
+    }
+    
+    @PostMapping("/")
+    public String add(@RequestParam String name, @RequestParam String isbn) {
+        Book book = new Book(name, isbn);
+        this.bookRepository.save(book);
+        return "redirect:/";
     }
 }
--- src/main/java/tip/domain/Book.java
+++ src/main/java/tip/domain/Book.java
@@ -2,10 +2,17 @@ package tip.domain;
 
 import javax.persistence.Entity;
 import org.springframework.data.jpa.domain.AbstractPersistable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 @Entity
 public class Book extends AbstractPersistable<Long> {
 
     private String name;
-    private String type;
+    private String isbn;
+    
 }
