a157cef8e5ed708e5351313f94a2185559dcb8fe
Ouzii <oskari.laaja@gmail.com>
2017-12-02 15:34:29 +0200

    Procfile+profile properties

---
+++ src/main/java/tip/ProductionConfiguration.java
@@ -0,0 +1,19 @@
+package tip;
+
+import javax.sql.DataSource;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("production")
+@Configuration
+public class ProductionConfiguration {
+    
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+}
