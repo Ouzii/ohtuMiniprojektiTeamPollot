870f69e008688f6fb68f0f7ea6fe21efb603ba1d
Ouzii <oskari.laaja@gmail.com>
2017-12-04 00:56:48 +0200

    Koitetaan vielä

---
+++ travis-merge.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+
+: "${BRANCHES_TO_MERGE_REGEX?}" "${BRANCH_TO_MERGE_INTO?}"
+: "${GITHUB_SECRET_TOKEN?}" "${GITHUB_REPO?}"
+
+export GIT_COMMITTER_EMAIL='travis@travis'
+export GIT_COMMITTER_NAME='Travis CI'
+
+if ! grep -q "$BRANCHES_TO_MERGE_REGEX" <<< "$TRAVIS_BRANCH"; then
+    printf "Current branch %s doesn't match regex %s, exiting\\n" \
+        "$TRAVIS_BRANCH" "$BRANCHES_TO_MERGE_REGEX" >&2
+    exit 0
+fi
+
+# Since Travis does a partial checkout, we need to get the whole thing
+repo_temp=$(mktemp -d)
+git clone "https://github.com/$GITHUB_REPO" "$repo_temp"
+
+# shellcheck disable=SC2164
+cd "$repo_temp"
+
+git pull "https://$GITHUB_SECRET_TOKEN@github.com/$GITHUB_REPO" "$TRAVIS_BRANCH"
+git push "https://$GITHUB_SECRET_TOKEN@github.com/$GITHUB_REPO" "$TRAVIS_BRANCH:master" >/dev/null 2>&1
+
+
