da338bdfe761acd29f408be344ac0f02eb1935e7
Ouzii <oskari.laaja@gmail.com>
2017-12-04 00:32:36 +0200

    Update travis-merge.sh

--- travis-merge.sh
+++ travis-merge.sh
@@ -22,8 +22,7 @@ cd "$repo_temp"
 printf 'Checking out %s\n' "$BRANCH_TO_MERGE_INTO" >&2
 git checkout "$BRANCH_TO_MERGE_INTO"
 
-printf 'Merging %s\n' "$TRAVIS_COMMIT" >&2
-git merge --ff-only "$TRAVIS_COMMIT"
+
 
 printf 'Pushing to %s\n' "$GITHUB_REPO" >&2
 
