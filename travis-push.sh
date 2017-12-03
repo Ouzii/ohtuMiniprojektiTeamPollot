c6a0d32b788387e905058db58edb5f522435f731
Ouzii <oskari.laaja@gmail.com>
2017-12-03 20:00:28 +0200

    Update travis-push.sh

--- travis-push.sh
+++ travis-push.sh
@@ -1,31 +1,21 @@
-#!/bin/bash -e
+#!/bin/sh
 
-: "${BRANCHES_TO_MERGE_REGEX?}" "${BRANCH_TO_MERGE_INTO?}"
-: "${GITHUB_SECRET_TOKEN?}" "${GITHUB_REPO?}"
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
 
-export GIT_COMMITTER_EMAIL='travis@travis'
-export GIT_COMMITTER_NAME='Travis CI'
+commit_website_files() {
+  git checkout -b gh-pages
+  git add . *.html
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
 
-if ! grep -q "$BRANCHES_TO_MERGE_REGEX" <<< "$TRAVIS_BRANCH"; then
-    printf "Current branch %s doesn't match regex %s, exiting\\n" \
-        "$TRAVIS_BRANCH" "$BRANCHES_TO_MERGE_REGEX" >&2
-    exit 0
-fi
+upload_files() {
+  git remote add origin-pages https://${GH_TOKEN}@github.com/MVSE-outreach/resources.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin-pages gh-pages 
+}
 
-# Since Travis does a partial checkout, we need to get the whole thing
-repo_temp=$(mktemp -d)
-git clone "https://github.com/$GITHUB_REPO" "$repo_temp"
-
-# shellcheck disable=SC2164
-cd "$repo_temp"
-
-printf 'Checking out %s\n' "$BRANCH_TO_MERGE_INTO" >&2
-git checkout "$BRANCH_TO_MERGE_INTO"
-
-printf 'Pushing to %s\n' "$GITHUB_REPO" >&2
-
-push_uri="https://$GITHUB_SECRET_TOKEN@github.com/$GITHUB_REPO"
-
-# Redirect to /dev/null to avoid secret leakage
-git push "$push_uri" "$BRANCH_TO_MERGE_INTO" >/dev/null 2>&1
-git push "$push_uri" :"$TRAVIS_BRANCH" >/dev/null 2>&1
+setup_git
+commit_website_files
+upload_files
