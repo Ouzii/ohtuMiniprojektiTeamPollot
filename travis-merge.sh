1580805a4a9ee9b10f0fbbb3eb0b2279f14d4a05
Ouzii <oskari.laaja@gmail.com>
2017-12-04 00:33:30 +0200

    Update travis-merge.sh

--- travis-merge.sh
+++ travis-merge.sh
@@ -32,5 +32,3 @@ push_uri="https://$GITHUB_SECRET_TOKEN@github.com/$GITHUB_REPO"
 git push "$push_uri" "$BRANCH_TO_MERGE_INTO" >/dev/null 2>&1
 git push "$push_uri" :"$TRAVIS_BRANCH" >/dev/null 2>&1
 
-git checkout -b development
-git push --all "https://$GITHUB_SECRET_TOKEN@github.com/$GITHUB_REPO" >/dev/null 2>&1
