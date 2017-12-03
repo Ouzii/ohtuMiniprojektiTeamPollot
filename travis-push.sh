d25f2c484768550ec6caef30a4376ac0db58b404
Ouzii <oskari.laaja@gmail.com>
2017-12-03 20:14:33 +0200

    Update travis-push.sh

--- travis-push.sh
+++ travis-push.sh
@@ -6,14 +6,14 @@ setup_git() {
 }
 
 commit_website_files() {
-  git checkout -b gh-pages
+  git checkout -b development
   git add . *.html
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
-  git remote add origin-pages https://${GH_TOKEN}@github.com/MVSE-outreach/resources.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin-pages gh-pages 
+  git remote add origin-pages https://${GH_TOKEN}@github.com/Ouzii/ohtuMiniprojektiTeamPollot.git > /dev/null 2>&1
+  git push --set-upstream origin master
 }
 
 setup_git
