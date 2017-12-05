1d6295569739aa37f8c48bb2a4072f557d5e01b1
Raul Becker <raul.becker@iki.fi>
2017-12-05 11:22:54 +0200

    stepdefsissa tauhkaa ja cache .travis.yml:lle

--- src/test/java/tip/Stepdefs.java
+++ src/test/java/tip/Stepdefs.java
@@ -18,7 +18,7 @@ public class Stepdefs {
 
     WebDriver driver;
     private int startCount;
-
+/*
     public void setChromePathForLocalTesting(boolean on) {
         if (on) {
             File file = new File("lib/chromedriver.exe");
@@ -26,10 +26,10 @@ public class Stepdefs {
         }
 
     }
-
+*/
     @Before
     public void setUp() {
-        setChromePathForLocalTesting(false); //True jos testit suoritetaan kotikoneella. Lisäksi on alustettava testinäytteet ja käynnistettävä testiserveri manuaalisesti.
+  //      setChromePathForLocalTesting(false); //True jos testit suoritetaan kotikoneella. Lisäksi on alustettava testinäytteet ja käynnistettävä testiserveri manuaalisesti.
         //False jos haluataan että travis build menee läpi!!!
 
         startCount = 0;
