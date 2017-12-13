164865610cd09eff414484171666046c051681ef
Ouzii <oskari.laaja@gmail.com>
2017-12-13 18:51:49 +0200

    Testit takasin

--- src/test/java/tip/ServerRule.java
+++ src/test/java/tip/ServerRule.java
@@ -1,27 +1,27 @@
-//package tip;
-//
-//import cucumber.api.cli.Main;
-//import org.junit.rules.ExternalResource;
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.context.ConfigurableApplicationContext;
-//
-//
-//public class ServerRule extends ExternalResource{
-//
-//    private final int port;
-//    ConfigurableApplicationContext app;
-//
-//    public ServerRule(int port) {
-//        this.port = port;
-//    }
-//
-//    @Override
-//    protected void before() throws Throwable {
-//        this.app = SpringApplication.run(Main.class);
-//    }
-//
-//    @Override
-//    protected void after() {
-//        app.close();
-//    }
-//}
+package tip;
+
+import cucumber.api.cli.Main;
+import org.junit.rules.ExternalResource;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+
+public class ServerRule extends ExternalResource{
+
+    private final int port;
+    ConfigurableApplicationContext app;
+
+    public ServerRule(int port) {
+        this.port = port;
+    }
+
+    @Override
+    protected void before() throws Throwable {
+        this.app = SpringApplication.run(Main.class);
+    }
+
+    @Override
+    protected void after() {
+        app.close();
+    }
+}
