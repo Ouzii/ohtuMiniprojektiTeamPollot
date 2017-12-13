029dfd02d8a855d96cf8a3507b0eee2735116082
Ouzii <oskari.laaja@gmail.com>
2017-12-13 18:37:22 +0200

    Heroku js

--- src/test/java/tip/RunCukesTest.java
+++ src/test/java/tip/RunCukesTest.java
@@ -1,13 +1,13 @@
-package tip;
-
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
-import org.junit.ClassRule;
-import org.junit.runner.RunWith;
-
-@RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"})
-public class RunCukesTest {
-//    @ClassRule
-//    public static ServerRule server = new ServerRule(8080);  ServerRule classia ei muka löydy vaikka sen pitäisi olla samassa pakkauksessa...
-}
+//package tip;
+//
+//import cucumber.api.CucumberOptions;
+//import cucumber.api.junit.Cucumber;
+//import org.junit.ClassRule;
+//import org.junit.runner.RunWith;
+//
+//@RunWith(Cucumber.class)
+//@CucumberOptions(plugin = {"pretty"})
+//public class RunCukesTest {
+////    @ClassRule
+////    public static ServerRule server = new ServerRule(8080);  ServerRule classia ei muka löydy vaikka sen pitäisi olla samassa pakkauksessa...
+//}
